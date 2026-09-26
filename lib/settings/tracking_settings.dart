ee31d1cffcff3ae9dd8c0fde7326eeffe0d8818b
neoenox <neoenox@gmail.com>
2026-09-26 23:50:48 +0900

    refactor: keep user settings separate from service diagnostics

--- lib/settings/tracking_settings.dart
+++ lib/settings/tracking_settings.dart
@@ -3,9 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class TrackingSettings {
   final SharedPreferencesAsync _preferences = SharedPreferencesAsync();
 
-  static const _trackingErrorKey = 'tracking_latest_error';
-  static const _locationStreamHealthyKey = 'location_stream_healthy';
-
   Future<bool> get onboardingCompleted async =>
       await _preferences.getBool('onboarding_completed') ?? false;
 
@@ -18,20 +15,4 @@ class TrackingSettings {
   Future<void> setAutoTrackingEnabled(bool value) =>
       _preferences.setBool('auto_tracking_enabled', value);
 
-  Future<String?> get latestTrackingError =>
-      _preferences.getString(_trackingErrorKey);
-
-  Future<void> setLatestTrackingError(String? message) async {
-    if (message == null) {
-      await _preferences.remove(_trackingErrorKey);
-    } else {
-      await _preferences.setString(_trackingErrorKey, message);
-    }
-  }
-
-  Future<bool> get locationStreamHealthy async =>
-      await _preferences.getBool(_locationStreamHealthyKey) ?? false;
-
-  Future<void> setLocationStreamHealthy(bool value) =>
-      _preferences.setBool(_locationStreamHealthyKey, value);
 }
